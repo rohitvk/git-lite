d9e90651c04956618f2208f71df2bf9a710898d1
rohit <rohit.born2code@gmail.com>
2018-11-02 09:33:20 +0530

    Update UserSearch.js

--- src/Components/UserSearch.js
+++ src/Components/UserSearch.js
@@ -7,7 +7,7 @@ const UserSearch = (props) => {
 return (
     <div className = "form-container">
         <form onSubmit={props.getGitData}>
-        <input className="userName" type="text" name="username"/>
+        <input className="userName" type="text" placeholder="Git username here" name="username"/>
         <button className="subBtn">Search</button>
       </form>
     </div>
